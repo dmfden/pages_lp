c7277b69429e6007c92496c0bf3055f0bb6aaab5
Denis Putintsev <denmfwk@gmail.com>
2024-03-25 22:00:47 +0200

    updates to quest lp

--- questgames/src/assets/js/main.js
+++ questgames/src/assets/js/main.js
@@ -15,4 +15,29 @@ closeButton.addEventListener('click', () => {
     dialog.close();
 });
 */
+function getSelectedFilter(filters) {
+    for (const key of filters) {
+        if (key.classList.contains('landing-filter__button_active')) {
+            return key;
+        }
+    }
+}
+
+const filter = document.querySelector('.landing-filter__games');
+const filtersButtons = filter.children;
+let selectedFilter = getSelectedFilter(filtersButtons);
+
+filter.addEventListener('click', (event) => {
+    const filterClicked = event.target.dataset.filter;
+    if (filterClicked === selectedFilter.dataset.filter) {
+        return;
+    }
+
+    for (const key of filtersButtons) {
+        key.classList.remove('landing-filter__button_active');
+    }
+    const activeButton = document.querySelector(`[data-filter=${filterClicked}]`);
+    activeButton.classList.add('landing-filter__button_active');
+    selectedFilter = getSelectedFilter(filtersButtons);
+});
 
